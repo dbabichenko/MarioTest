f021de629bbabbb3b033a41034f250833652c0aa
dbabichenko <dmb72@pitt.edu>
2026-02-09 15:32:45 -0500

    Added sound effects and triggers

--- Assets/_Scripts/MushroomTrigger.cs
+++ Assets/_Scripts/MushroomTrigger.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MushroomTrigger : MonoBehaviour
 {
-    
+    public TMP_Text scoreText;
+    public GameObject painManager;
+    private int score = 0;
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("COllided with something" + other.name);
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player has triggered the mushroom!");
+
+            score = int.Parse(scoreText.text);
+            score = score + 1;
+            scoreText.text = score.ToString();
+
+            painManager.GetComponent<AudioSource>().Play();
             // Add additional logic here, e.g., increase score, play sound, etc.
             Destroy(gameObject);
         }
